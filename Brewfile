f868a3fe35a7732d3db9ff13ec91256e62aa1c64
ohyunbok <ohyunbok@mz.co.kr>
2023-05-09 00:13:06 +0900

    개발 환경 자동화 스크립트

---
+++ Brewfile
@@ -0,0 +1,62 @@
+tap "adoptopenjdk/openjdk"
+tap "gcenx/wine"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+tap "homebrew/services"
+brew "autojump"
+brew "cask"
+brew "colima"
+brew "git"
+brew "mas"
+brew "neofetch"
+brew "nvm"
+brew "zsh-autosuggestions"
+brew "zsh-syntax-highlighting"
+cask "appcleaner"
+cask "bettertouchtool"
+cask "cheatsheet"
+cask "coconutbattery"
+cask "dbeaver-community"
+cask "deepl"
+cask "fig"
+cask "gemini"
+cask "google-chrome"
+cask "google-drive"
+cask "iina"
+cask "iterm2"
+cask "itsycal"
+cask "jetbrains-toolbox"
+cask "keka"
+cask "keyboard-cleaner"
+cask "microsoft-edge"
+cask "notion"
+cask "onedrive"
+cask "onyx"
+cask "popclip"
+cask "postman"
+cask "raycast"
+cask "slack"
+cask "sourcetree"
+cask "telegram"
+cask "visual-studio-code"
+mas "CloudMounter", id: 1130254674
+mas "com.alice.mac.GetPlainText", id: 508368068
+mas "Dropover", id: 1355679052
+mas "Friendly Streaming", id: 553245401
+mas "G App Launcher", id: 1543803459
+mas "iBar", id: 6443843900
+mas "iRightMouse", id: 1497428978
+mas "iRightMouse Lite", id: 1552554632
+mas "Irvue", id: 1039633667
+mas "iShot", id: 1485844094
+mas "Keynote", id: 409183694
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "RunCat", id: 1429033973
+mas "ScanTexter", id: 1635954549
+mas "Vimari", id: 1480933944
+mas "Xcode", id: 497799835
+mas "네이버 MYBOX", id: 975815190
+mas "유니콘", id: 1231935892
+mas "카카오톡", id: 869223134
---
+++ install.sh
@@ -0,0 +1,58 @@
+# !/bin/bash
+
+# Homebrew 설치 여부 확인
+if ! which brew
+then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# 스크립트 내에서 일부 sudo 권한이 필요한 명령을 수행하기 위해 root 패스워드를 입력
+# sudo 권한이 필요한 이유 : cask로 설치한 애플리케이션을 바로 실행하기 위해 다운로드 된 파일에 대한 격리 속성 제거 작업
+read -r -s -p "[sudo] sudo password for $(whoami):" pass
+
+# BrewFile 실행 명령어
+brew bundle --file=./Brewfile
+
+# cask로 다운로드시 웹에서 다운로드한 것과 동일하기 때문에 실행을 하면 Security 문제로 실행되지 않음
+# cask로 설치한 애플리케이션을 바로 실행하기 위해 다운로드 된 파일에 대한 격리 속성 제거 작업 명령어
+sudo xattr -dr com.apple.quarantine /Applications/AppCleaner
+sudo xattr -dr com.apple.quarantine /Applications/BetterTouchTool
+sudo xattr -dr com.apple.quarantine /Applications/CheatSheet
+sudo xattr -dr com.apple.quarantine /Applications/coconutBattery
+sudo xattr -dr com.apple.quarantine /Applications/DBeaver
+sudo xattr -dr com.apple.quarantine /Applications/DeepL
+sudo xattr -dr com.apple.quarantine /Applications/Fig
+sudo xattr -dr com.apple.quarantine /Applications/Gemini\ 2
+sudo xattr -dr com.apple.quarantine /Applications/Google\ Chrome
+sudo xattr -dr com.apple.quarantine /Applications/Google\ Drive
+sudo xattr -dr com.apple.quarantine /Applications/IINA
+sudo xattr -dr com.apple.quarantine /Applications/iTerm
+sudo xattr -dr com.apple.quarantine /Applications/Itsycal
+sudo xattr -dr com.apple.quarantine /Applications/JetBrains\ Toolbox
+sudo xattr -dr com.apple.quarantine /Applications/Keka
+sudo xattr -dr com.apple.quarantine /Applications/Keyboard\ Cleaner
+sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Edge
+sudo xattr -dr com.apple.quarantine /Applications/Notion
+sudo xattr -dr com.apple.quarantine /Applications/OneDrive
+sudo xattr -dr com.apple.quarantine /Applications/OnyX
+sudo xattr -dr com.apple.quarantine /Applications/PopClip
+sudo xattr -dr com.apple.quarantine /Applications/Postman
+sudo xattr -dr com.apple.quarantine /Applications/Raycast
+sudo xattr -dr com.apple.quarantine /Applications/Slack
+sudo xattr -dr com.apple.quarantine /Applications/Sourcetree
+sudo xattr -dr com.apple.quarantine /Applications/Telegram
+sudo xattr -dr com.apple.quarantine /Applications/Visual\ Studio\ Code
+
+# install font
+cp -a ./fonts/. ~/Library/Fonts
+
+# configure zsh
+chmod 755 ./zsh/install.sh
+./zsh/install.sh
+
+# copy iterm2 configuration
+chmod 755 ./iterm2/install.sh
+./iterm2/install.sh
+
+# 설치 성공 완료 메세지 노출
+printf '\n install success! \n'
